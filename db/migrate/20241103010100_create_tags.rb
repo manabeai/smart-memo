cc611dfbb586db510fa2af2d509f3bfa3057a8e0
HackberryA3 <1107a3@gmail.com>
2024-11-03 02:52:02 +0000

    Added migration files

---
+++ db/migrate/20241103010100_create_tags.rb
@@ -0,0 +1,9 @@
+class CreateTags < ActiveRecord::Migration[7.2]
+  def change
+    create_table :tags do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_01_012140) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_03_010111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "memo_tags", force: :cascade do |t|
+    t.bigint "memo_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["memo_id"], name: "index_memo_tags_on_memo_id"
+    t.index ["tag_id"], name: "index_memo_tags_on_tag_id"
+  end
+
+  create_table "memos", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_memos_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "tags", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tags_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.string "provider"
+    t.string "uid"
+    t.string "access_token"
+    t.string "access_secret"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email", "uid"], name: "index_users_on_email_and_uid", unique: true
+  end
+
+  add_foreign_key "memo_tags", "memos"
+  add_foreign_key "memo_tags", "tags"
+  add_foreign_key "memos", "users"
+  add_foreign_key "tags", "users"
 end
