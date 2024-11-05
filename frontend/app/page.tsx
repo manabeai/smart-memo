7694f5f8344fd446fd83b4b7b3ae532f12ea4007
HackberryA3 <1107a3@gmail.com>
2024-11-05 08:16:17 +0000

    処理をクライアントサイドに移行

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,25 +1,40 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import api from '@/utils/index.ts'; // axiosインスタンスをインポート
 
-export default async function Posts() {
-  let response = await api.get('/posts'); // baseURLが適用されるのでURLが短くなる
-  let posts = response.data;
+let init : boolean = false;
+export default function Posts() {
+	const [data, setData] = useState(null);
 
-  return (
-    <ScrollArea className="h-[400px] w-full rounded-md border p-4">
-      <h2 className="text-2xl font-bold mb-4">投稿</h2>
-      <ul className="grid gap-4">
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Card>
-              <CardHeader>
-                <CardTitle>{post.title}</CardTitle>
-              </CardHeader>
-            </Card>
-          </li>
-        ))}
-      </ul>
-    </ScrollArea>
-  );
+	useEffect(() => {
+		const fetchData = async () => {
+			console.log("APIを叩きました");
+		  init = true;
+			const result = await api.get("/posts");
+			setData(result.data)
+		};
+		if (!init) {
+			fetchData();
+		}
+	}, [])
+
+	return (
+		<ScrollArea className="h-[400px] w-full rounded-md border p-4">
+			<h2 className="text-2xl font-bold mb-4">投稿</h2>
+			<ul className="grid gap-4">
+				{data && data.map((post) => (
+					<li key={post.id}>
+						<Card>
+							<CardHeader>
+								<CardTitle>{post.title}</CardTitle>
+							</CardHeader>
+						</Card>
+					</li>
+				))}
+			</ul>
+		</ScrollArea>
+	);
 }
