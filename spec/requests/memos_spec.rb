0aa637ba9d417e6ed83360850fbaac16164eae12
manabeai <100462113+manabeai@users.noreply.github.com>
2024-11-07 20:37:42 +0900

    rspecに変更 (#2)
    * rspecに変更
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update ci.yml
    
    * rspecを追加

--- spec/requests/memos_spec.rb
+++ spec/requests/memos_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Memos", type: :request do
-  let(:user) { create(:user) }  # ユーザーを作成
+  let(:user) { User.find_or_create_by(id: 1) { |u| u.email = 'test@example.com'; u.password = 'password' } }
 
 
   describe "GET /memos" do
     it "returns a successful response" do
-      get "/memos"
+      get "/memos", headers: { 'ACCEPT' => 'application/json' }
       # debugger
       expect(response).to have_http_status(:success)
     end
 
     it "returns the correct number of memos" do
       create_list(:memo, 3, user: user)
-      get "/memos", headers: { Authorization: "Bearer #{user.access_token}" }
+      get "/memos"
+      debugger
       expect(JSON.parse(response.body).size).to eq(3)
     end
   end
