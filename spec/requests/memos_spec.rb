b70a2895e683ad4ef20a08968a36fe7f675b135e
manabeai <matsu.devtool@gmail.com>
2024-11-07 12:50:49 +0000

    deleteのテストを追加

--- spec/requests/memos_spec.rb
+++ spec/requests/memos_spec.rb
@@ -6,12 +6,14 @@ RSpec.describe "Memos", type: :request do
   describe "GET /memos" do
     it "returns a successful response" do
       get "/memos"
+
       expect(response).to have_http_status(:success)
     end
 
     it "returns the correct number of memos" do
       create_list(:memo, 3, user: user)
       get "/memos"
+
       expect(JSON.parse(response.body).size).to eq(3)
     end
   end
@@ -21,6 +23,7 @@ RSpec.describe "Memos", type: :request do
       create_list(:default_tag, 4)
       create_list(:tag, 5, user: user)
       post "/memos", params: { title: "Test", content: "Test" }
+
       expect(response).to have_http_status(:success)
     end
 
@@ -29,6 +32,7 @@ RSpec.describe "Memos", type: :request do
       create_list(:default_tag, 4)
       create_list(:tag, 5, user: user)
       post "/memos", params: { title: "Test", content: "Test" }
+
       expect(JSON.parse(response.body)["title"]).to eq("Test")
       expect(JSON.parse(response.body)["content"]).to eq("Test")
       expect(JSON.parse(response.body)["tags"]).to be_an_instance_of(Array)
@@ -43,6 +47,7 @@ RSpec.describe "Memos", type: :request do
       initial_tag_count = Tag.count
       tag = Tag.all.sample
       post "/memos/create", params: { title: "test", content: "Testcontent", tags: [ { name: DefaultTag.all.sample, is_user_defined: nil }, { name: tag.name, is_user_defined: tag.user } ] }
+
       expect(JSON.parse(response.body)["title"]).to eq("test")
       expect(Memo.count) == initial_memo_count + 1
       expect(Tag.count)  == initial_tag_count + 1
@@ -57,6 +62,7 @@ RSpec.describe "Memos", type: :request do
       Tagging.create(memo: sample_memo, tag: Tag.first)
       Tagging.create(memo: sample_memo, tag: Tag.second)
       patch "/memos/#{Memo.first.id}", params: { id: sample_memo.id, title: "patch", content: "change", tags: [ { id: Tag.second.id, name: Tag.second.name }, { id: Tag.last.id, name: Tag.last.name } ] }
+
       expect(response).to have_http_status(:success)
     end
 
@@ -68,11 +74,30 @@ RSpec.describe "Memos", type: :request do
       Tagging.create(memo: sample_memo, tag: Tag.second)
 
       patch "/memos/#{sample_memo.id}", params: { id: sample_memo.id, title: "patch", content: "change", tags: [ { id: Tag.second.id, name: Tag.second.name }, { id: Tag.last.id, name: Tag.last.name } ] }
+
       expect(Memo.find(sample_memo.id).title).to eq("patch")
       expect(Memo.find(sample_memo.id).content).to eq("change")
       expect(Memo.find(sample_memo.id).tags.size).to eq(2)
       expect(Memo.find(sample_memo.id).tags.pluck(:id)).to eq([ Tag.second.id, Tag.last.id ])
       expect(Memo.find(sample_memo.id).tags.pluck(:name)).to eq([ Tag.second.name, Tag.last.name ])
+    end
+  end
+
+  describe "DELETE /memos/:id" do
+    it "returns a successful response" do
+      create_list(:memo, 5, user: user)
+      delete "/memos/#{Memo.first.id}"
+      expect(response).to have_http_status(204)
+    end
+
+    it "delete the memo" do
+      create_list(:memo, 5, user: user)
+      initial_memo_count = Memo.count
+      initial_memo_id = Memo.first.id
+      delete "/memos/#{Memo.first.id}"
+
+      expect(Memo.count).to eq(initial_memo_count - 1)
+      expect(Memo.where(id: initial_memo_id)).to be_empty
     end
   end
 end
