73a558ec429e8f06b7c0001083f27562b477283e
manabeai <100462113+manabeai@users.noreply.github.com>
2024-11-13 21:16:00 +0900

    Feature/fix appearance (#50)
    * layoutのデザインを追加
    
    * コンポーネントをpagesとmemo-cardとmemo-listに分離
    
    * エディターを編集
    
    * プリフライトリクエストが通らない
    
    * 削除と作成を追加
    
    * 見た目を少し調整
    
    ---------
    
    Co-authored-by: HackberryA3 <1107a3@gmail.com>

--- frontend/components/text-editor.tsx
+++ frontend/components/text-editor.tsx
@@ -8,13 +8,13 @@ import { Badge } from "@/components/ui/badge"
 import { toast } from "@/hooks/use-toast"
 import { Button } from "./ui/button"
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea"
@@ -23,101 +23,103 @@ import { Memo, Tag } from '@/components/memo-card'
 
   
 const FormSchema = z.object({
-    title: z
-      .string(),
-    content: z
-      .string()
-      .min(1, {
-        message: "何も入力されていません",
-    }),
+	title: z
+		.string(),
+	content: z
+		.string()
+		.min(1, {
+			message: "何も入力されていません",
+		}),
 });
 
 type suggestedTag = {
-    name: string
-    is_user_defined: number | null
+	name: string
+	is_user_defined: number | null
 }
 
 interface PostMemo {
-    title: string
-    content: string
+	title: string
+	content: string
 };
 
 interface CreateMemo extends PostMemo {
-    tags: suggestedTag[]
+	tags: suggestedTag[]
 };
 
 interface TextEditorProps {
-    onMemoCreate: (memo: Memo) => void;
+	onMemoCreate: (memo: Memo) => void;
 }
 
 export function TextEditor({ onMemoCreate }: TextEditorProps) {
 
-    const [newMemo, setNewMemo] = useState<PostMemo>({ title: '', content: ''})
-    const [suggestedTags, setSuggestedTags] = useState<suggestedTag[]>([])
-    const [selectedTags, setSelectedTags] = useState<suggestedTag[]>([])
-    const handleSubmit = async () => {
-        const response = await api.post('/memos', newMemo);
-        setSuggestedTags(response["data"]["tags"]);
-        setSelectedTags([])
-        console.log(response);
-      }
-      
-      const handleMemoCreate = async () => {
-        const response = await api.post('/memos/create', { ...newMemo, tags:selectedTags });
-        setSuggestedTags([])
-        setSelectedTags([])
-        console.log('メモ作成に成功' + response)
-        onMemoCreate(response.data);
-    }
+	const [newMemo, setNewMemo] = useState<PostMemo>({ title: '', content: '' })
+	const [suggestedTags, setSuggestedTags] = useState<suggestedTag[]>([])
+	const [selectedTags, setSelectedTags] = useState<suggestedTag[]>([])
+	const handleSubmit = async () => {
+		const response = await api.post('/memos', newMemo);
+		setSuggestedTags(response["data"]["tags"]);
+		setSelectedTags([])
+		console.log(response);
+	}
 
-    const handleTagClick = (tag: suggestedTag) => {
-        setSelectedTags(prevTags => 
-          prevTags.some(t => t.name === tag.name)
-            ? prevTags.filter(t => t.name !== tag.name)
-            : [...prevTags, tag]
-        )
-      }
-    const isTagSelected = (tag: Tag) => selectedTags.some(t => t.name === tag.name)
+	const handleMemoCreate = async () => {
+		const response = await api.post('/memos/create', { ...newMemo, tags: selectedTags });
+		setSuggestedTags([])
+		setSelectedTags([])
+		console.log('メモ作成に成功' + response)
+		onMemoCreate(response.data);
+	}
 
-    return (
-        <div className="w-2/3 space-y-4">
-        <Input
-          type="text"
-          placeholder="タイトル"
-          value={newMemo.title}
-          onChange={(e) => setNewMemo({ ...newMemo, title: e.target.value })}
-          className="mb-2"
-        />
-        <Textarea
-          placeholder="メモの内容"
-          value={newMemo.content}
-          onChange={(e) => setNewMemo({ ...newMemo, content: e.target.value })}
-          className="w-full p-2 border rounded mb-2"
-          rows={3}
-        />
-        <Button onClick={handleSubmit}>AI</Button>
-        <Button onClick={handleMemoCreate}>作成</Button>
-  
-        {suggestedTags.length > 0 && (
-          <div className="mt-4">
-            <h3 className="text-lg font-semibold mb-2">おすすめのタグ：</h3>
-            <div className="flex flex-wrap gap-2">
-              {suggestedTags.map((tag, index) => (
-                <Badge
-                  key={index}
-                  variant={tag.is_user_defined === null ? "secondary" : "default"}
-                  className={`cursor-pointer ${isTagSelected(tag) ? 'ring-2 ring-blue-500' : ''}`}
-                  onClick={() => handleTagClick(tag)}
-                >
-                  {tag.name}
-                  {tag.is_user_defined === null && (
-                    <span className="ml-1 text-xs">(新規)</span>
-                  )}
-                </Badge>
-              ))}
-            </div>
-          </div>
-        )}
-      </div>
-    )
-  }
+	const handleTagClick = (tag: suggestedTag) => {
+		setSelectedTags(prevTags =>
+			prevTags.some(t => t.name === tag.name)
+				? prevTags.filter(t => t.name !== tag.name)
+				: [...prevTags, tag]
+		)
+	}
+	const isTagSelected = (tag: Tag) => selectedTags.some(t => t.name === tag.name)
+
+	return (
+		<div className="w-2/3 space-y-4 mx-auto">
+			<Input
+				type="text"
+				placeholder="タイトル"
+				value={newMemo.title}
+				onChange={(e) => setNewMemo({ ...newMemo, title: e.target.value })}
+				className="mb-2"
+			/>
+			<Textarea
+				placeholder="メモの内容"
+				value={newMemo.content}
+				onChange={(e) => setNewMemo({ ...newMemo, content: e.target.value })}
+				className="w-full p-2 border rounded mb-2"
+				rows={3}
+			/>
+			<div className='flex justify-start gap-x-3'>
+				<Button onClick={handleSubmit}>AI</Button>
+				<Button onClick={handleMemoCreate}>作成</Button>
+			</div>
+
+			{suggestedTags.length > 0 && (
+				<div className="mt-4">
+					<h3 className="text-lg font-semibold mb-2">おすすめのタグ：</h3>
+					<div className="flex flex-wrap gap-2">
+						{suggestedTags.map((tag, index) => (
+							<Badge
+								key={index}
+								variant={tag.is_user_defined === null ? "secondary" : "default"}
+								className={`cursor-pointer ${isTagSelected(tag) ? 'ring-2 ring-blue-500' : ''}`}
+								onClick={() => handleTagClick(tag)}
+							>
+								{tag.name}
+								{tag.is_user_defined === null && (
+									<span className="ml-1 text-xs">(新規)</span>
+								)}
+							</Badge>
+						))}
+					</div>
+				</div>
+			)}
+		</div>
+	)
+}
