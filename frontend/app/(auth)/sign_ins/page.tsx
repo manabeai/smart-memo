a2a14f6b66353d1346183952c331c529f8ec94f4
Twil3akine <twil3akine@gmail.com>
2024-11-15 00:51:41 +0900

    超簡易的なログイン画面の作成

--- frontend/app/(auth)/sign_ins/page.tsx
+++ frontend/app/(auth)/sign_ins/page.tsx
@@ -1,11 +1,32 @@
 // app/(auth)/sign_ins/page.tsx
-'use client';
+'use client'
 
-const SignInPage = () => {
+import api from '@/utils/index';
+import { Button } from '@/components/ui/button';
+import "@/app/globals.css";
+
+const handlePost = async () => {
+  try {
+    const response = await api.post('/sign_ins');
+    console.log('Response:', response.data);
+    window.location.href = '/';
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+
+const Page = () => {
   return (
-    <>
-    </>
+		<div className={`w-[50%] h-[40%] flex flex-col items-center justify-around`}>
+			<p className={`text-8xl text-[#000]`}>MemoritAI</p>
+			<Button
+				onClick={handlePost}
+				className={"bg-[#000] hover:bg-[#000]/60 w-auto h-auto rounded"}>
+				Get Start
+			</Button>
+		</div>
   );
 };
 
-export default SignInPage;
+export default Page;
